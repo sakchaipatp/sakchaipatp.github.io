e8ddfd59f3c8298f6a4420358be434010ccb4d96
sakchaipatpBU <sakchai.patp@bumail.net>
2026-02-26 01:55:34 +0700

    lots of features, up to zoom & drag

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Smooth scroll
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", e => {
-    e.preventDefault();
-    const target = document.querySelector(link.getAttribute("href"));
-    target.scrollIntoView({ behavior: "smooth" });
+    const href = link.getAttribute("href");
+
+    // e.preventDefault();
+
+    if (href.startsWith("#")) {
+      e.preventDefault();
+      const target = document.querySelector(href);
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    }
   });
 });
 
@@ -16,6 +24,7 @@ const children = [
 ];
 
 const svg = document.getElementById("skill-lines");
+const tree = document.querySelector(".skill-tree");
 
 function drawLines() {
   svg.innerHTML = "";
@@ -47,3 +56,108 @@ function drawLines() {
 window.addEventListener("load", drawLines);
 window.addEventListener("resize", drawLines);
 
+// Drag skill tree
+let isDragging = false;
+let startX, startY;
+let currentX = 0;
+let currentY = 0;
+
+tree.addEventListener("mousedown", e => {
+  isDragging = true;
+  startX = e.clientX - currentX;
+  startY = e.clientY - currentY;
+  e.preventDefault();
+});
+
+window.addEventListener("mousemove", e => {
+  if (!isDragging) return;
+
+  currentX = e.clientX - startX;
+  currentY = e.clientY - startY;
+
+  updateTransform();
+});
+
+window.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+window.addEventListener("mouseleave", () => {
+  isDragging = false;
+});
+
+// zoom control
+let scale = 1;
+const minScale = 0.8;
+const maxScale = 1.2;
+
+function updateTransform() {
+  applyBounds();
+  tree.style.transform = `translate(${currentX}px, ${currentY}px) scale(${scale})`;
+  drawLines();
+}
+
+// Zoom with Scroll Mouse
+document.querySelector(".skill-tree-container")
+  .addEventListener("wheel", e => {
+    e.preventDefault();
+
+    if (e.deltaY < 0) {
+      scale += 0.1;
+    } else {
+      scale -= 0.1;
+    }
+
+    scale = Math.min(maxScale, Math.max(minScale, scale));
+    updateTransform();
+  });
+
+  // Zoom with Button
+  document.getElementById("zoom-in").addEventListener("click", () => {
+  scale += 0.1;
+  scale = Math.min(maxScale, scale);
+  updateTransform();
+});
+
+document.getElementById("zoom-out").addEventListener("click", () => {
+  scale -= 0.1;
+  scale = Math.max(minScale, scale);
+  updateTransform();
+});
+
+// Clamp
+function clamp(value, min, max) {
+  return Math.min(max, Math.max(min, value));
+}
+
+function applyBounds() {
+  const container = document.querySelector(".skill-tree-container");
+
+  const containerRect = container.getBoundingClientRect();
+  const treeRect = tree.getBoundingClientRect();
+
+  const scaledWidth = tree.offsetWidth * scale;
+  const scaledHeight = tree.offsetHeight * scale;
+
+  const margin = 200;
+
+  const minX = containerRect.width - scaledWidth;
+  const minY = containerRect.height - scaledHeight;
+
+  const maxX = margin;
+  const maxY = margin;
+
+  currentX = clamp(currentX, minX, maxX);
+  currentY = clamp(currentY, minY, maxY);
+}
+
+
+window.addEventListener("load", () => {
+  const container = document.querySelector(".skill-tree-container");
+
+  currentX = (container.clientWidth - tree.offsetWidth) / 2;
+  currentY = 40;
+
+  updateTransform();
+});
+//End Skill
